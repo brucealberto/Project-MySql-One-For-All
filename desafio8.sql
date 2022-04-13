f49ef795d1db64d6711f0e86ed50262b05879c3e
Bruce <brucealberto2@gmail.com>
2022-04-13 20:32:00 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,2 @@
+SELECT art.nome_artista AS 'artista', alb.nome_album AS 'album' FROM SpotifyClone.artista AS art
+INNER JOIN SpotifyClone.album AS alb ON art.artista_id = alb.artista_id WHERE art.nome_artista = 'Walter Phoenix' ORDER BY alb.nome_album;
